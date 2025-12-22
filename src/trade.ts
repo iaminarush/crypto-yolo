d7262adb920cd72cad105c943965580af1c5aaf7
Patrick <patrickcheng1995@gmail.com>
2025-12-22 17:50:11 +0800

    wip

--- src/trade.ts
+++ src/trade.ts
@@ -1,8 +1,12 @@
-export async function fetchWeights(url: string) {
+import { Resource } from "sst";
+
+export async function fetchWeights() {
+  const url = new URL("https://api.robotwealth.com/v1/yolo/weights");
+  const params = new URLSearchParams();
+
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
   return response.json();
 }
-
