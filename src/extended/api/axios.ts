f695de7050be3fc3701abe1ae58f8d17c70d9aa9
Patrick <patrickcheng1995@gmail.com>
2026-01-28 17:46:35 +0800

    format baseurl

--- src/extended/api/axios.ts
+++ src/extended/api/axios.ts
@@ -1,48 +1,48 @@
-import axios from 'axios'
+import axios from "axios";
 
-import { parseJsonWithBigNumber } from '../utils/json.ts'
+import { parseJsonWithBigNumber } from "../utils/json.ts";
 
 const safeParseResponse = (data: unknown) => {
-  if (!data || typeof data !== 'string') {
-    return undefined
+  if (!data || typeof data !== "string") {
+    return undefined;
   }
 
   try {
-    return parseJsonWithBigNumber(data)
+    return parseJsonWithBigNumber(data);
   } catch {
-    return undefined
+    return undefined;
   }
-}
+};
 
 export const axiosClient = axios.create({
   paramsSerializer: {
     indexes: null,
   },
-})
+});
 
 axiosClient.interceptors.response.use(
   (response) => {
-    return response
+    return response;
   },
   (error) => {
     if (axios.isAxiosError(error)) {
       return Promise.reject({
-        url: error.response.config.url,
-        status: error.response.status,
-        data: error.response.data,
-      })
+        url: error.response?.config.url,
+        status: error.response?.status,
+        data: error.response?.data,
+      });
     }
 
-    return Promise.reject(error)
+    return Promise.reject(error);
   },
-)
+);
 
-axiosClient.defaults.transformResponse = [safeParseResponse]
+axiosClient.defaults.transformResponse = [safeParseResponse];
 
 export const setHost = (baseUrl: string) => {
-  axiosClient.defaults.baseURL = `https://${baseUrl}`
-}
+  axiosClient.defaults.baseURL = baseUrl;
+};
 
 export const setApiKey = (apiKey: string) => {
-  axiosClient.defaults.headers.common['x-api-key'] = apiKey
-}
+  axiosClient.defaults.headers.common["x-api-key"] = apiKey;
+};
