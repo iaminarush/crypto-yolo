01805175d61330bf13530592adfd6bcde8088569
Patrick <patrickcheng1995@gmail.com>
2026-01-28 17:47:21 +0800

    change api format

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const EXTENDED_API = "https://api.starknet.extended.exchange/api";
+export const EXTENDED_API = "https://api.starknet.extended.exchange";
 export const ROBOTWEALTH_API = "https://api.robotwealth.com/v1/yolo";
+export const EXTENDED_VAULT_ID = "110816";
