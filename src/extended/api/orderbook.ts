bfebf55ff323b8e73a5d7a3fba737aa995374521
Patrick <patrickcheng1995@gmail.com>
2026-01-28 17:46:50 +0800

    init get orderbook

---
+++ src/extended/api/orderbook.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { axiosClient } from "./axios.ts";
+import { zodDecimal } from "../utils/zod.ts";
+
+const OrderbookLevelSchema = z.object({
+  qty: zodDecimal(),
+  price: zodDecimal(),
+});
+
+export const OrderbookSchema = z.object({
+  market: z.string(),
+  bid: OrderbookLevelSchema.array(),
+  ask: OrderbookLevelSchema.array(),
+});
+
+export type Orderbook = z.infer<typeof OrderbookSchema>;
+
+export const getOrderbook = async (ticker: string) => {
+  const { data } = await axiosClient.get<unknown>(`/api/v1/info/markets/${ticker}/orderbook`);
+
+  return OrderbookSchema.parse(data);
+};
