96482ac11e498a478513299c1622fa51636c6665
Patrick <patrickcheng1995@gmail.com>
2026-02-25 17:50:02 +0800

    return entire array instead of first

--- src/extended/api/markets.ts
+++ src/extended/api/markets.ts
@@ -1,12 +1,13 @@
-import { axiosClient } from './axios.ts'
-import { MarketsResponseSchema } from './markets.schema.ts'
+import { axiosClient } from "./axios.ts";
+import { MarketsResponseSchema } from "./markets.schema.ts";
 
-export const getMarket = async (marketName: string) => {
-  const { data } = await axiosClient.get<unknown>('/api/v1/info/markets', {
-    params: {
-      market: [marketName],
-    },
-  })
+export const getMarket = async () => {
+  const { data } = await axiosClient.get<unknown>("/api/v1/info/markets", {
+    // params: {
+    //   market: [marketName],
+    // },
+  });
 
-  return MarketsResponseSchema.parse(data).data[0]
-}
+  // return MarketsResponseSchema.parse(data).data[0];
+  return MarketsResponseSchema.parse(data).data;
+};
