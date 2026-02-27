e4b90cab733d53cec374ec72b839baf522a0b63c
Patrick <patrickcheng1995@gmail.com>
2026-02-27 17:44:32 +0800

    add api

--- src/extended/api/markets.ts
+++ src/extended/api/markets.ts
@@ -1,13 +1,18 @@
 import { axiosClient } from "./axios.ts";
 import { MarketsResponseSchema } from "./markets.schema.ts";
 
-export const getMarket = async () => {
+export const getMarket = async (marketName: string) => {
   const { data } = await axiosClient.get<unknown>("/api/v1/info/markets", {
-    // params: {
-    //   market: [marketName],
-    // },
+    params: {
+      market: [marketName],
+    },
   });
 
-  // return MarketsResponseSchema.parse(data).data[0];
+  return MarketsResponseSchema.parse(data).data[0];
+};
+
+export const getMarkets = async () => {
+  const { data } = await axiosClient.get<unknown>("/api/v1/info/markets");
+
   return MarketsResponseSchema.parse(data).data;
 };
