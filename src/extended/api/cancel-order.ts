11707624df8684e17a808aee2eea64b299234098
Patrick <patrickcheng1995@gmail.com>
2026-03-02 17:58:17 +0800

    fix url typo and add zod parsing

--- src/extended/api/cancel-order.ts
+++ src/extended/api/cancel-order.ts
@@ -1,6 +1,14 @@
+import z from "zod";
 import { axiosClient } from "./axios.ts";
 
 export const cancelOrder = async (orderId: string) => {
-  const { data } = await axiosClient.delete<unknown>(`/api/v1/user/orders/${orderId}`);
-  return data;
+  const { data } = await axiosClient.delete<unknown>(
+    `/api/v1/user/order/${orderId}`,
+  );
+  return CancelOrderResponseSchema.parse(data);
 };
+
+const CancelOrderResponseSchema = z.object({
+  status: z.enum(["OK"]),
+  data: z.object(),
+});
