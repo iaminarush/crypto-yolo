e6599202452b23e50389e8b9caf3335c966f0a50
Patrick <patrickcheng1995@gmail.com>
2026-03-04 17:50:08 +0800

    extract supabase_url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const EXTENDED_API = "https://api.starknet.extended.exchange";
 export const ROBOTWEALTH_API = "https://api.robotwealth.com/v1/yolo";
 export const EXTENDED_VAULT_ID = "110816";
+export const SUPABASE_URL = "https://lapkwtulywsjfjogngcx.supabase.co";
