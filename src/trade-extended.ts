3aa95647839cbfd22a0c97ae2a9357cf388cc31e
Patrick <patrickcheng@tutanota.com>
2026-03-06 21:03:22 +0800

    change runtime to be less than aws lambda timeout

--- src/trade-extended.ts
+++ src/trade-extended.ts
@@ -18,7 +18,7 @@ import { clamp, fetchAndParse } from "./util.ts";
 import { createLimitOrder } from "./extended/create-limit-order.ts";
 
 const SLEEP_MS = 1000;
-const MAX_RUNTIME_MS = 15 * 60 * 1000;
+const MAX_RUNTIME_MS = 10 * 60 * 1000;
 
 export const handler: Handler = async () => {
   await init();
