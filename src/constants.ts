551f502ad1b3de0aa5887b5a711c66f9e15d6184
Patrick <patrickcheng1995@gmail.com>
2026-03-12 16:46:04 +0800

    add timestamp id

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const EXTENDED_API = "https://api.starknet.extended.exchange";
 export const ROBOTWEALTH_API = "https://api.robotwealth.com/v1/yolo";
 export const EXTENDED_VAULT_ID = "110816";
 export const SUPABASE_URL = "https://lapkwtulywsjfjogngcx.supabase.co";
+export const TIMESTAMP_ID = 1;
