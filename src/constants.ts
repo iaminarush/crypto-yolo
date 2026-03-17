2f4e18b88ed88cbb8810064cfb81665fc06db82c
Patrick <patrickcheng1995@gmail.com>
2026-03-17 18:55:54 +0800

    extract slippage

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const ROBOTWEALTH_API = "https://api.robotwealth.com/v1/yolo";
 export const EXTENDED_VAULT_ID = "110816";
 export const SUPABASE_URL = "https://lapkwtulywsjfjogngcx.supabase.co";
 export const TIMESTAMP_ID = 1;
+export const SLIPPAGE = 0.005;
