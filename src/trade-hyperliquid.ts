f7093485ada9b8db2a700482784387cb9233a3ad
Patrick <patrickcheng1995@gmail.com>
2026-03-17 17:53:25 +0800

    wip

--- src/trade-hyperliquid.ts
+++ src/trade-hyperliquid.ts
@@ -1,19 +1,27 @@
 import {
+  type AllMidsResponse,
   type ClearinghouseStateResponse,
+  ExchangeClient,
   HttpTransport,
   InfoClient,
   type MetaResponse,
 } from "@nktkas/hyperliquid";
+import { SymbolConverter } from "@nktkas/hyperliquid/utils";
 import type { Handler } from "aws-lambda";
-import BigNumber from "bignumber.js";
+import BN from "bignumber.js";
 import type { Database } from "database.types";
 import { Resource } from "sst";
+import type { Hex } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { getConfig, getTickers, getWeightsAndVolatilities } from "./api";
+import { createLimitOrder } from "./hyperliquid/create-limit-order";
 
+const SLEEP_MS = 1000;
 const MAX_RUNTIME_MS = 10 * 60 * 1000;
+const MINIMUM_ORDER_VALUE = BN(10);
 
 export const handler: Handler = async () => {
-  const WALLET = Resource.HYPERLIQUID_WALLET.value;
+  const startTime = Date.now();
   // const startMessage = `Hyperliquid Lambda Started!`;
   // ky.post(
   //   `https://api.telegram.org/bot${Resource.TELEGRAM_TOKEN.value}/sendMessage`,
@@ -25,9 +33,12 @@ export const handler: Handler = async () => {
   //   },
   // ).catch(console.error);
   //
-  const startTime = Date.now();
+  const WALLET = Resource.HYPERLIQUID_WALLET.value as Hex;
   const transport = new HttpTransport();
   const client = new InfoClient({ transport });
+  const wallet = privateKeyToAccount(Resource.HYPERLIQUID_KEY.value as Hex);
+  const converter = await SymbolConverter.create({ transport });
+  const exchange = new ExchangeClient({ transport, wallet });
 
   const config = await getConfig("hyperliquid");
   const volAndWeight = await getWeightsAndVolatilities(config);
@@ -56,6 +67,8 @@ export const handler: Handler = async () => {
     for (const [ticker, desiredPosition] of tickersToRebalance) {
       //TODO: Implement trading loop
       const order = await getOpenOrder(client, ticker);
+      const allMids = await client.allMids();
+
       if (!order) {
         const { assetPositions: updatedPositions } =
           await client.clearinghouseState({
@@ -64,12 +77,89 @@ export const handler: Handler = async () => {
         const currentPosition = updatedPositions.find(
           (p) => p.position.coin === ticker,
         );
-        const orderSize = calculateOrderSize(
+        const { size, side } = calculateOrderSize(
           desiredPosition,
-          BigNumber(currentPosition ? currentPosition.position.szi : 0),
+          BN(currentPosition ? currentPosition.position.szi : 0),
+          allMids,
         );
+
+        if (size.gt(0)) {
+          await createLimitOrder({ ticker, size, side });
+        } else {
+          tickersToRebalance.delete(ticker);
+        }
       } else {
+        const book = await client.l2Book({ coin: ticker });
+        const bestPrice = book?.levels[order.side === "B" ? 0 : 1]?.[0].px;
+
+        if (bestPrice && !BN(order.limitPx).eq(bestPrice)) {
+          try {
+            await exchange.cancel({
+              cancels: [
+                { a: converter.getAssetId(order.coin) || "", o: order.oid },
+              ],
+            });
+          } catch (error) {
+            console.error(
+              `Cancel failed for ${order.coin} ${order.oid}:`,
+              error,
+            );
+            continue;
+          }
+
+          const updatedPositions = await client.clearinghouseState({
+            user: WALLET,
+          });
+          const currentPosition = updatedPositions.assetPositions.find(
+            (p) => p.position.coin === ticker,
+          );
+
+          const { size, side } = calculateOrderSize(
+            desiredPosition,
+            currentPosition ? BN(currentPosition.position.szi) : BN(0),
+            allMids,
+          );
+
+          if (size.gt(0)) {
+            await createLimitOrder({ ticker, size, side });
+          } else {
+            tickersToRebalance.delete(ticker);
+          }
+        }
       }
+      await new Promise((resolve) => setTimeout(resolve, SLEEP_MS));
+    }
+  }
+
+  const openOrders = await client.openOrders({ user: WALLET });
+  await exchange.cancel({
+    cancels: openOrders.map((o) => ({
+      a: converter.getAssetId(o.coin) || "",
+      o: o.oid,
+    })),
+  });
+  const { assetPositions: postTradePositions } =
+    await client.clearinghouseState({ user: WALLET });
+
+  const tickersToMarketOrder = filterTickersToRebalance(
+    desiredPositions,
+    postTradePositions,
+  );
+
+  for (const [ticker, desiredPosition] of tickersToMarketOrder) {
+    const currentPosition = postTradePositions.find(
+      (p) => p.position.coin === ticker,
+    );
+    const allMids = await client.allMids();
+    const { size, side } = calculateOrderSize(
+      desiredPosition,
+      BN(currentPosition ? currentPosition.position.szi : 0),
+      allMids,
+    );
+
+    //TODO: finish post order
+    if (size.gt(0)) {
+      await createMarketOrder({ ticker, size, side });
     }
   }
 };
@@ -93,8 +183,8 @@ const calculateDesiredPositions = (
 
     const tokenAllocation =
       "token_allocation" in vw
-        ? (vw.token_allocation as unknown as BigNumber)
-        : new BigNumber(0);
+        ? (vw.token_allocation as unknown as BN)
+        : new BN(0);
 
     const isPositive = tokenAllocation.gte(0);
 
@@ -105,18 +195,15 @@ const calculateDesiredPositions = (
       exchangeTicker,
       desiredSize: tokenAllocation,
       upperBound: tokenAllocation.times(
-        BigNumber(isPositive ? config.trade_buffer : -config.trade_buffer).plus(
-          1,
-        ),
+        BN(isPositive ? config.trade_buffer : -config.trade_buffer).plus(1),
       ),
       lowerBound: tokenAllocation.times(
-        BigNumber(isPositive ? -config.trade_buffer : config.trade_buffer).plus(
-          1,
-        ),
+        BN(isPositive ? -config.trade_buffer : config.trade_buffer).plus(1),
       ),
       minOrderSizeChange: market
         ? getMinOrderSizeChange(market.szDecimals)
-        : BigNumber(0),
+        : BN(0),
+      szDecimals: market ? market.szDecimals : 1,
     };
   });
 };
@@ -128,7 +215,7 @@ const filterTickersToRebalance = (
   currentPositions: ClearinghouseStateResponse["assetPositions"],
 ) => {
   const positionMap = new Map(
-    currentPositions.map((p) => [p.position.coin, BigNumber(p.position.szi)]),
+    currentPositions.map((p) => [p.position.coin, BN(p.position.szi)]),
   );
 
   const result = new Map<string, TDesiredPosition>();
@@ -150,34 +237,72 @@ const filterTickersToRebalance = (
   return result;
 };
 
-//TODO: Currently WIP
 function calculateOrderSize(
   desiredPosition: TDesiredPosition,
-  currentPosition: BigNumber,
-) {
-  if (
-    currentPosition.gte(desiredPosition.lowerBound) &&
-    currentPosition.lte(desiredPosition.upperBound)
-  ) {
-    return { size: BigNumber(0), side: "BUY" };
-  }
-  if (currentPosition.lt(desiredPosition.lowerBound)) {
-    const gap = desiredPosition.lowerBound.minus(currentPosition);
+  currentPosition: BN,
+  allMids: AllMidsResponse,
+): { size: BN; side: "BUY" | "SELL" } {
+  const { szDecimals, lowerBound, upperBound } = desiredPosition;
+  const midPrice = allMids[desiredPosition.exchangeTicker];
+  const minOrdersize = MINIMUM_ORDER_VALUE.div(midPrice).decimalPlaces(
+    szDecimals,
+    BN.ROUND_UP,
+  );
 
-    if (gap.lt(desiredPosition.minOrderSizeChange)) {
-      return { size: BigNumber(0), side: "BUY" };
-    }
+  if (currentPosition.gte(lowerBound) && currentPosition.lte(upperBound)) {
+    return { size: BN(0), side: "BUY" };
   }
+  if (currentPosition.lt(lowerBound)) {
+    const gap = lowerBound.minus(currentPosition);
+
+    const size = gap.lt(minOrdersize) ? minOrdersize : gap;
+
+    const roundedUp = roundToDecimal(size, szDecimals, BN.ROUND_UP);
+    const roundedDown = roundToDecimal(size, szDecimals, BN.ROUND_DOWN);
+
+    if (currentPosition.plus(roundedUp).gt(upperBound)) {
+      if (currentPosition.plus(roundedDown).gt(upperBound)) {
+        return { size: BN(0), side: "BUY" };
+      } else {
+        return { size: roundedDown, side: "BUY" };
+      }
+    }
+
+    return { size: roundedUp, side: "BUY" };
+  }
+
+  if (currentPosition.gt(desiredPosition.upperBound)) {
+    const gap = desiredPosition.upperBound
+      .minus(currentPosition)
+      .absoluteValue();
+
+    const size = gap.lt(minOrdersize) ? minOrdersize : gap;
+
+    const roundedUp = roundToDecimal(size, szDecimals, BN.ROUND_UP);
+    const roundedDown = roundToDecimal(size, szDecimals, BN.ROUND_DOWN);
+
+    if (currentPosition.minus(roundedUp).gt(lowerBound)) {
+      if (currentPosition.minus(roundedDown).gt(lowerBound)) {
+        return { size: BN(0), side: "SELL" };
+      } else {
+        return { size: roundedDown, side: "SELL" };
+      }
+    }
+
+    return { size: roundedUp, side: "SELL" };
+  }
+
+  return { size: BN(0), side: "BUY" };
 }
 
-function getMinOrderSizeChange(szDecimals: number): BigNumber {
-  return new BigNumber(1).dividedBy(new BigNumber(10).pow(szDecimals));
+function getMinOrderSizeChange(szDecimals: number): BN {
+  return new BN(1).dividedBy(new BN(10).pow(szDecimals));
 }
 
 function roundToDecimal(
-  value: BigNumber,
+  value: BN,
   szDecimals: number,
-  roundingMode?: BigNumber.RoundingMode,
+  roundingMode?: BN.RoundingMode,
 ) {
   return value.decimalPlaces(szDecimals, roundingMode);
 }
