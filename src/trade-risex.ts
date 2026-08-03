e447f4f4d90d94c47acb679b8be36fd9756e5928
Patrick <patrickcheng1995@gmail.com>
2026-08-03 17:51:02 +0800

    wip

--- src/trade-risex.ts
+++ src/trade-risex.ts
@@ -1,4 +1,5 @@
 import type { Handler } from "aws-lambda";
+import BN from "bignumber.js";
 import type { Database } from "database.types";
 import { InfoClient, type Market } from "risex-client";
 import {
@@ -7,13 +8,17 @@ import {
   getTickers,
   getWeights,
 } from "./api";
+import { Resource } from "sst";
 
 export const handler: Handler = async () => {
-  const info = new InfoClient();
+  const WALLET = Resource.HYPERLIQUID_WALLET.value;
+  const info = new InfoClient({ baseUrl: "https://api.rise.trade" });
   const config = await getConfig("risex");
   const weights = await getWeights();
   const markets = await info.getMarkets();
   const tickers = await getTickers();
+
+  const positions = await info.getAllPositions(WALLET);
 
   const filteredMarkets = tickers
     .filter(
@@ -25,12 +30,17 @@ export const handler: Handler = async () => {
 
   const volAndWeight = await getDemeanedWeightsAndVols(config, filteredMarkets);
 
-  // const desiredPositions = calculateDesiredPositions(
-  //   volAndWeight,
-  //   tickers,
-  //   config,
-  //   markets,
-  // );
+  const desiredPositions = calculateDesiredPositions(
+    volAndWeight,
+    tickers,
+    config,
+    markets,
+  );
+
+  const tickersToRebalance = filterTickersToRebalance(
+    desiredPositions,
+    assetPositions,
+  );
 };
 
 const calculateDesiredPositions = (
@@ -44,4 +54,30 @@ const calculateDesiredPositions = (
       .filter((t) => markets.some((m) => m.market_id === t.risex_ticker))
       .map((t) => [t.rbw_ticker, t.risex_ticker]),
   );
+
+  return volAndWeight.map((vw) => {
+    const exchangeTicker = tickerMap.get(vw.ticker);
+    if (!exchangeTicker) throw new Error(`No risex ticker for ${vw.ticker}`);
+
+    const tokenAllocation = vw.token_allocation;
+
+    const isPositive = tokenAllocation.gte(0);
+
+    const market = markets.find((m) => m.market_id === exchangeTicker);
+    if (!market) throw new Error(`No risex market for ${vw.ticker}`);
+
+    return {
+      rwTicker: vw.ticker,
+      exchangeTicker,
+      desiredSize: tokenAllocation,
+      upperBound: tokenAllocation.times(
+        BN(isPositive ? config.trade_buffer : -config.trade_buffer).plus(1),
+      ),
+      lowerBound: tokenAllocation.times(
+        BN(isPositive ? -config.trade_buffer : config.trade_buffer).plus(1),
+      ),
+      minOrderSizeChange: market.config.min_order_size,
+      szDecimals: market.config.step_size,
+    };
+  });
 };
