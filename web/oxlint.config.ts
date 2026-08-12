68fe995bd7dfc0005afb386d6b3a3310dc7e7c38
Patrick <patrickcheng1995@gmail.com>
2026-08-12 13:34:08 +0800

    remove better-t-stack

--- web/oxlint.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "oxlint";
-import core from "ultracite/oxlint/core";
-import react from "ultracite/oxlint/react";
-import tanstack from "ultracite/oxlint/tanstack";
-export default defineConfig({
-  extends: [core, react, tanstack],
-  ignorePatterns: core.ignorePatterns,
-});
