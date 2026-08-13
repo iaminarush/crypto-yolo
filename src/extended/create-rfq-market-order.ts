967f491b58662dc4ad3fdf154d22df1cd005473e
Patrick <patrickcheng1995@gmail.com>
2026-08-13 11:05:54 +0800

    init

---
+++ src/extended/create-rfq-market-order.ts
@@ -0,0 +1,61 @@
+import { SLIPPAGE } from "@/constants";
+import { getFees } from "./api/fees";
+import { getMarket } from "./api/markets";
+import { placeRFQOrder } from "./api/order";
+import { getStarknetDomain } from "./api/starknet";
+import { init } from "./init";
+import { Order } from "./models/order";
+import type { OrderSide } from "./models/order.types";
+import { createOrderContext } from "./utils/create-order-context";
+import { Decimal } from "./utils/number";
+import { roundToMinChange } from "./utils/round-to-min-change";
+
+export const createMarketOrder = async ({
+  ticker,
+  size,
+  side,
+}: {
+  ticker: string;
+  size: Decimal;
+  side: OrderSide;
+}) => {
+  const { starkPrivateKey, vaultId } = await init();
+
+  const market = await getMarket(ticker);
+  const fees = await getFees({ marketName: ticker });
+  const starknetDomain = await getStarknetDomain();
+
+  const ctx = createOrderContext({
+    market,
+    fees,
+    starknetDomain,
+    vaultId,
+    starkPrivateKey,
+  });
+
+  const order = Order.create({
+    marketName: ticker,
+    orderType: "MARKET",
+    side,
+    amountOfSynthetic: roundToMinChange(
+      size,
+      market.tradingConfig.minOrderSizeChange,
+      Decimal.ROUND_DOWN,
+    ),
+    price: roundToMinChange(
+      side === "BUY"
+        ? market.marketStats.markPrice.times(Decimal(1).plus(SLIPPAGE))
+        : market.marketStats.markPrice.times(Decimal(1).minus(SLIPPAGE)),
+      market.tradingConfig.minPriceChange,
+      Decimal.ROUND_DOWN,
+    ),
+    timeInForce: "IOC",
+    reduceOnly: false,
+    postOnly: false,
+    ctx,
+  });
+
+  const result = await placeRFQOrder({ order });
+
+  return result;
+};
