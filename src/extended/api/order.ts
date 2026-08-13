21d9cc7a1a80e0837beb5606902df4c54847c7a2
Patrick <patrickcheng1995@gmail.com>
2026-08-13 11:06:06 +0800

    add rfq order

--- src/extended/api/order.ts
+++ src/extended/api/order.ts
@@ -1,8 +1,20 @@
-import { axiosClient } from './axios.ts'
-import { PlacedOrderResponseSchema } from './orders.schema.ts'
+import { axiosClient } from "./axios.ts";
+import { PlacedOrderResponseSchema } from "./orders.schema.ts";
 
 export const placeOrder = async (args: { order: object }) => {
-  const { data } = await axiosClient.post<unknown>('/api/v1/user/order', args.order)
+  const { data } = await axiosClient.post<unknown>(
+    "/api/v1/user/order",
+    args.order,
+  );
 
-  return PlacedOrderResponseSchema.parse(data).data
-}
+  return PlacedOrderResponseSchema.parse(data).data;
+};
+
+export const placeRFQOrder = async (args: { order: object }) => {
+  const { data } = await axiosClient.post<unknown>(
+    "/api/v1/user/order/rfq",
+    args.order,
+  );
+
+  return PlacedOrderResponseSchema.parse(data).data;
+};
